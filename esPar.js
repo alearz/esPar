ddb20fd00458c7994922ee4f13287165132a8299
Alexis Arzamendia <arzalexis7@gmail.com>
2020-12-21 10:25:31 -0300

    esPar

---
+++ esPar.js
@@ -0,0 +1,8 @@
+let paresImpares = function(numero){
+
+    if( numero !== 0){ 
+        numero % 2 === 0? console.log('El numero es par'): console.log('El numero es impar'); 
+    } else {
+        return 'error';
+    }
+ }
